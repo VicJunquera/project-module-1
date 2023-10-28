ecd689a8b844c2af99f0f02855503a8ea28bd521
Hector Hernandez <hectorahbravo@gmail.com>
2023-10-28 11:24:41 +0200

    Bullets, start Game and Player move

--- js/bullet.js
+++ js/bullet.js
@@ -1,17 +1,17 @@
 class Bullet {
-  constructor(container, x, y, playerRotation) {
+  constructor(container, player, angle) {
     this.container = container;
-    this.width = 5;
-    this.height = 5;
-    this.speed = 5;
-    this.x = x;
-    this.y = y;
-    this.rotation = playerRotation;
+    this.width = 14;
+    this.height = 14;
+    this.speed = 15;
+    this.x = player.x + player.width / 2;
+    this.y = player.y + player.height / 2;
+    this.angle = angle;
 
     this.element = document.createElement("div");
     this.element.style.position = "absolute";
-    this.element.style.backgroundColor = "red";
-    this.element.style.borderRadius = "100%";
+    this.element.style.backgroundImage = "url(./assets/bullet.gif)";
+    this.element.style.objectFit = "cover";
 
     this.element.style.width = `${this.width}px`;
     this.element.style.height = `${this.height}px`;
@@ -24,7 +24,7 @@ class Bullet {
   }
 
   move() {
-    const radians = (this.rotation * Math.PI) / 180;
+    const radians = this.angle;
     const dx = Math.cos(radians) * this.speed;
     const dy = Math.sin(radians) * this.speed;
 
