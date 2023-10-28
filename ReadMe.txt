930cb4eb10e2db9fa6b044d7213e18e4bd856702
Victor Junquera <victorjunquera@eventalista.es>
2023-10-28 12:36:21 +0200

    enemies with player and bullets collisions

--- ReadMe.txt
+++ ReadMe.txt
@@ -8,10 +8,13 @@ Player
 Movimiento con WASD + bomba con espacio
 	O movimiento con flechas + bomba con 0
 
+Colisiones con enemigos y con disparos de enemigo grande
+
 Disparo
 	Desde la posición del player
 Disparo con el click izquierdo a la dirección donde está el puntero
 Un número de vidas
+Colisiones con enemigos
 
 Score
 	Contador de puntos por derrotar enemigos
@@ -19,9 +22,13 @@ Score
 
 Enemigos
 Distintos tipos de comportamiento
-	Enemigo con avance lineal (vertical u horizontal)
+	Enemigo con avance lineal horizontal
+	Enemigo con avance lineal vertical
+	Enemigo que se mueva despacio y aguante varios disparos (más grande)
+		Que dispare	
+
 	Enemigo que persiga al jugador
-	Enemigo que aguante varios disparos (más grande)	
+
 
 Item
 Bomba que acaba con los enemigos (limpia el board de enemigos)
--- js/game.js
+++ js/game.js
@@ -18,10 +18,67 @@ class Game {
 
   }
 
+  cleanup() {
+    this.enemies.forEach((enemy) => {
+      const inBoard = enemy.x + enemy.width > 0;
+      if (!inBoard) {
+        console.log("enemy.element", enemy.element);
+        enemy.element.remove();
+      }
+    });
+
+    const filteredEnemies = this.enemies.filter((enemy) => {
+      return enemy.x + enemy.width > 0;
+    });
+
+    this.enemies = filteredEnemies;
+  }
+
+
+checkCollisions() {
+    // Enemy - player collision
+
+    const collidedEnemy = this.enemies.find((enemy) => {
+      return enemy.didCollide(this.player);
+      
+    });
+
+    if (collidedEnemy) {
+      this.enemies = this.enemies.filter((enemy) => {
+        return enemy !== collidedEnemy;
+      });
+
+      collidedEnemy.element.style.display = "none";
+      this.player.hits--;
+
+    }
+
+    // bullet - enemy collision
+
+    this.player.bullets.find((bullet) => {
+      return this.enemies.find((enemy) => {
+        if (enemy.didCollide(bullet)) {
+          enemy.element.remove();
+
+          this.enemies = this.enemies.filter((en) => {
+            return en !== enemy;
+          });
+
+          bullet.element.remove();
+
+          this.player.bullets = this.player.bullets.filter((bul) => {
+            return bul !== bullet;
+          });
+        }
+      });
+    });
+  }
   update() {
     this.player.move();
     this.enemies.forEach((enemy) => {
       enemy.move();
     });
+    this.checkCollisions()
+    this.cleanup()
   }
 }
