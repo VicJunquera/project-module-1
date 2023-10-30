b6a38edd350d9ff52d7fb298bbe9481e2b2bd3ed
Hector Hernandez <hectorahbravo@gmail.com>
2023-10-30 18:38:32 +0100

    class score done

--- js/enemy.js
+++ js/enemy.js
@@ -1,48 +1,45 @@
 class Enemy {
-    constructor(container) {
-        this.container = container;
-        this.width = 50;
-        this.height = 50;
-        this.x = this.container.offsetWidth
-        this.y = Math.floor(Math.random() * (this.container.offsetHeight ));
-        
-        this.vx = -10;
+  constructor(container) {
+    this.container = container;
+    this.width = 60;
+    this.height = 40;
+    this.x = this.container.offsetWidth;
+    this.y = Math.floor(Math.random() * this.container.offsetHeight);
 
-        this.element = document.createElement("div");
-        this.element.style.position = "absolute";
-        this.element.style.background = `url(./assets/enemy3.gif)`;
-        this.element.style.backgroundSize = "cover";
-        this.element.style.backgroundPosition = "center";
-        this.element.style.width = `${this.width}px`;
-        this.element.style.height = `${this.height}px`;
-        this.element.style.left = `${this.x}px`;
-        this.element.style.top = `${this.y}px`;
+    this.vx = -5;
 
+    this.element = document.createElement("div");
+    this.element.style.position = "absolute";
+    this.element.style.background = `url(./assets/enemy3.gif)`;
+    this.element.style.backgroundSize = "cover";
+    this.element.style.backgroundPosition = "center";
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
 
-        this.container.appendChild(this.element);
-        this.move();
-  
+    this.container.appendChild(this.element);
+    this.move();
+  }
+
+  move() {
+    this.x += this.vx;
+    this.element.style.left = `${this.x}px`;
+  }
+
+  didCollide(obstacle) {
+    const enemyRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+
+    if (
+      enemyRect.left < obstacleRect.right &&
+      enemyRect.right > obstacleRect.left &&
+      enemyRect.top < obstacleRect.bottom &&
+      enemyRect.bottom > obstacleRect.top
+    ) {
+      return true;
+    } else {
+      return false;
     }
-
-    move() {
-        this.x += this.vx;
-        this.element.style.left = `${this.x}px`;
-      }
-    
-      didCollide(obstacle) {
-        const enemyRect = this.element.getBoundingClientRect();
-        const obstacleRect = obstacle.element.getBoundingClientRect();
-    
-        if (
-          enemyRect.left < obstacleRect.right &&
-          enemyRect.right > obstacleRect.left &&
-          enemyRect.top < obstacleRect.bottom &&
-          enemyRect.bottom > obstacleRect.top
-        ) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+  }
 }
-
--- js/game.js
+++ js/game.js
@@ -2,26 +2,94 @@ class Game {
   constructor(container) {
     this.container = container;
     this.player = new Player(this.container);
-    this.enemies = []
-    
+    this.enemies = [];
+    this.score = new Score(this.container, this.player.hits);
+
     setInterval(() => {
       this.enemies.push(new Enemy(this.container));
-      }, 2000);
-  
+    }, 2000);
   }
 
   start() {
     this.intervalId = setInterval(() => {
       this.update();
     }, 1000 / 30);
-
-
   }
 
+  cleanup() {
+    this.enemies.forEach((enemy) => {
+      const inBoard = enemy.x + enemy.width > 0;
+      if (!inBoard) {
+        console.log("enemy.element", enemy.element);
+        enemy.element.remove();
+      }
+    });
+
+    const filteredEnemies = this.enemies.filter((enemy) => {
+      return enemy.x + enemy.width > 0;
+    });
+
+    this.enemies = filteredEnemies;
+  }
+
+  checkCollisions() {
+    // Enemy - player collision
+
+    const collidedEnemy = this.enemies.find((enemy) => {
+      return enemy.didCollide(this.player);
+    });
+
+    if (collidedEnemy) {
+      this.enemies = this.enemies.filter((enemy) => {
+        return enemy !== collidedEnemy;
+      });
+      this.score.score = 10;
+
+      collidedEnemy.element.style.display = "none";
+      this.player.hits--;
+    }
+
+    // bullet - enemy collision
+
+    this.player.bullets.find((bullet) => {
+      return this.enemies.find((enemy) => {
+        if (enemy.didCollide(bullet)) {
+          enemy.element.remove();
+
+          this.enemies = this.enemies.filter((en) => {
+            return en !== enemy;
+          });
+
+          bullet.element.remove();
+
+          this.player.bullets = this.player.bullets.filter((bul) => {
+            return bul !== bullet;
+          });
+        }
+      });
+    });
+  }
+  didCollide(obstacle) {
+    const enemyRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+
+    if (
+      enemyRect.left < obstacleRect.right &&
+      enemyRect.right > obstacleRect.left &&
+      enemyRect.top < obstacleRect.bottom &&
+      enemyRect.bottom > obstacleRect.top
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   update() {
     this.player.move();
     this.enemies.forEach((enemy) => {
       enemy.move();
     });
+    this.checkCollisions();
+    this.cleanup();
   }
 }
